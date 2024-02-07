cf18748a5340e7176ebe7444918c8dbb63386695
kratuwu <kratuwu@gmail.com>
2024-02-07 22:23:43 +0700

    remove unused import

--- src/main/java/com/example/demo/service/SalarySurveyService.java
+++ src/main/java/com/example/demo/service/SalarySurveyService.java
@@ -1,19 +1,15 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.SalarySurveyEntity;
-import com.example.demo.mapper.SalarySurveyMapper;
 import com.example.demo.repository.SalarySurveyRepository;
 import com.google.common.base.CaseFormat;
-import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Field;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static com.example.demo.utils.FieldFilterUtil.filterByFields;
 import static java.util.Objects.nonNull;
